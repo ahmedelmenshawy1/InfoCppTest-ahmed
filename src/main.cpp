a433cd12d83cdde76e6ad31e3951cfa501e65f87
Esraa <esraa.mahmoud.ismail@gmail.com>
2025-01-30 02:44:55 +0000

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include "RectangleIntersection.h"
+#include <filesystem>
+#include <memory>
+
+int main(int argc, char *argv[])
+{
+    bool correct_flag{};
+
+    if (argc < 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <json_file>\n";
+        return 1;
+    }
+
+    std::ifstream file(argv[1]);
+    std::string fileName(argv[1]);
+
+    if (!file.is_open() && std::filesystem::path(fileName).extension() == ".json")
+    {
+        std::cerr << "Failed to open file: " << argv[1] << "\n";
+        return 1;
+    }
+
+    RectangleIntersection ri(file);
+    correct_flag = ri.printRectangles();
+    if (!correct_flag)
+    {
+        std::cerr << "Failed in print Rectangles \n";
+        return 1;
+    }
+    correct_flag = ri.findIntersections();
+    if (!correct_flag)
+    {
+        std::cerr << "Failed in find Intersections \n";
+        return 1;
+    }
+    return 0;
+}
